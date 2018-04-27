7b417bdaf2cd218f7ab6e284e164a024ae3e407c
James Schofield Hayes <james.s.hayes@gmail.com>
2018-04-27 16:07:18 -0600

    added check for valid XML but not TBX

--- TBXSpyglassWebBridge.php
+++ TBXSpyglassWebBridge.php
@@ -1,32 +1,32 @@
 <?php
 
     // check the TBX-ness of a file
-		
+
     $temp_file_name = $_FILES['upload']['tmp_name'];
     if ($_FILES["upload"]["error"] > 0)
     {
         echo "<p>Error: " . $_FILES["file"]["error"] . "</p>\n";
     }
     $out_file_name = $temp_file_name . '.log';
-    
+
     // Define which version of Perl we will use
-    
+
     $perl = "~/.plenv/versions/5.18.0/bin/perl";
     $lib = "~/perl5/lib/perl5";
     $script = "./main.pl";
-    
+
     $reroute_stderr = "2>$out_file_name";
-        
-    $command = "$perl ". "-I $lib " . 
+
+    $command = "$perl ". "-I $lib " .
         escapeshellarg($script). ' ' .
-        escapeshellarg($temp_file_name) . ' -s ' . 
+        escapeshellarg($temp_file_name) . ' -s ' .
             $reroute_stderr;
-    
+
     $ret_val = 0;
-    
+
     // Execute the command, store terminal output in the $printed_output variable
     exec($command, $printed_output, $ret_val);
-    
+
     //if $ret_val is not 0 or if the log file wasn't created, there was a problem
     if(($ret_val != 0) || (!file_exists($out_file_name)) ){
         #print problems
@@ -41,8 +41,7 @@
         foreach ($printed_output as $line)
         {
             print "<p>$line</p>";
-        }
-        
-        print "<br/>";
-        print "<p>Go back in browser to return to TBX Spyglass utility.</p>";
+        }    
     }
+    print "<br/>";
+    print "<p>Go back in browser to return to TBX Spyglass utility.</p>";
--- main.pl
+++ main.pl
@@ -7,20 +7,24 @@ sub main
 {
     #THIS LINE DOES NOT WORK ON THE SERVER    
     #die "File does not have a '.tbx' extension.\n" if $_[0] !~ /\.tbx$/;
+    my $isTBX = 0;
     
     my $twig = XML::Twig->new(
         twig_handlers =>
         {
             tbx => sub {
                     printf "File appears to be a 2018 TBX file with dialect: '%s'", $_->att('type');
+                    $isTBX = 1;
                 },
             martif => sub {
-                printf "File appears to be a 2008 TBX file."
+                printf "File appears to be a 2008 TBX file.";
+                $isTBX = 1;
             }
         }
     );
     
     die "File does not appear to be valid XML.\n" unless $twig->safe_parsefile($_[0]);
+    print "File appears to be valid XML, but does not appear to be TBX." if $isTBX != 0;
 }
 
 die "Missing target file.\n" if (@ARGV < 2);
