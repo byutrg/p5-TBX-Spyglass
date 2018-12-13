0f7e0cdb09929d354cd7dda26edd094881f3af0a
James Schofield Hayes <james.s.hayes@gmail.com>
2018-12-13 14:28:59 -0700

    added file extension check and <?xml header check

--- main.pl
+++ main.pl
@@ -5,17 +5,20 @@ use XML::Twig;
 
 sub main
 {
-    #THIS LINE DOES NOT WORK ON THE SERVER    
+    my ($file) = @_;
+	#THIS LINE DOES NOT WORK ON THE SERVER    
     #die "File does not have a '.tbx' extension.\n" if $_[0] !~ /\.tbx$/;
     
     ## ordered by severity
     my %messages = (
+		not_xml => "This file does not appear to be an XML or TBX file.",
+		bad_ext => "This file does not appear to have the correct file extension.",
         malformed_xml => "File does not appear to be well-formed XML.",
         malformed_tbx => "File appears to be well-formed XML, but does not appear to be TBX.",
         invalid_tbx => "File claims to be TBX, but does not appear valid.",
-        2008 => "File appears to be a 2008 TBX file.",
-        bad_2018 => "File appears to be a 2018, but has no valid dialect.",
-        2018 => "File appears to be a 2018 TBX file with dialect:",
+        v2 => "File appears to be a 2008 TBX file.",
+        bad_v3 => "File appears to be a 2019 TBX (v3) file, but has no valid dialect.",
+        v3 => "File appears to be a 2019 TBX (v3) file with dialect:",
     );
     
     
@@ -24,21 +27,21 @@ sub main
         {
             ## TBX-Min originally had a mandatory upper-case <TBX> root element, but this is not legal in any other case
             TBX => sub {
-                if ($_->att('dialect') eq "TBX-Min") { print $messages{'2008'}."\n" }
+                if ($_->att('dialect') eq "TBX-Min") { print $messages{'v2'}."\n" }
                 else { print $messages{'invalid_tbx'}."\n" }
             },
             ## 2008 TBX has <martif> lower-case.
             martif => sub {
-                print $messages{'2008'}."\n";
+                print $messages{'v2'}."\n";
             },
             ## 2018 TBX files have a mandator lower-case <tbx> root element
             tbx => sub {
                 if ($_->att('type') =~ /^TBX-.+?/)
                 {
-                    printf $messages{'2018'}." '%s'\n", $_->att('type');    
+                    printf $messages{'v3'}." '%s'\n", $_->att('type');    
                 }
                 else {
-                    print $messages{'bad_2018'}."\n";
+                    print $messages{'bad_v3'}."\n";
                 }
             },
             'MARTIF' => sub {
@@ -47,7 +50,22 @@ sub main
         }
     );
     
-    die $messages{'malformed_xml'}."\n" unless $twig->safe_parsefile($_[0]);
+	open(my $fh, '<', $file);
+	my $firstline;
+	
+	while (<$fh>)
+	{
+		if ($_ =~ /\w/g)
+		{
+			die $messages{'not_xml'} unless $_ =~ /<\?xml/i;
+			last;
+		}
+	}
+	
+	
+	
+	die $messages{'bad_ext'}."\n" unless $file =~ /\.(xml|tbxm?)$/i;
+    die $messages{'malformed_xml'}."\n" unless $twig->safe_parsefile($file);
 }
 
 die "Missing target file.\n" if (@ARGV < 1);
